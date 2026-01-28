4f0f95ae68be57457764d19d0607edfb089c9104
anascacais <anascacais@gmail.com>
2026-01-28 20:07:46 +0100

    update bigquery code structure

--- code/bigquery_create.py
+++ code/bigquery_create.py
@@ -10,23 +10,29 @@ PROJECT_ID = os.getenv("PROJECT_ID")
 DATASET = os.getenv("DATASET_BQ")
 REGION = os.getenv("REGION")
 
-client = bigquery.Client(project=PROJECT_ID)
 
-# Create dataset
-dataset_id = f"{PROJECT_ID}.{DATASET}"
-dataset = bigquery.Dataset(dataset_id)
-dataset.location = REGION
+def main():
 
-dataset = client.create_dataset(dataset, exists_ok=True)
-print(f"Dataset {dataset_id} created in {dataset.location}")
+    client = bigquery.Client(project=PROJECT_ID)
+
+    # Create dataset
+    dataset_id = f"{PROJECT_ID}.{DATASET}"
+    dataset = bigquery.Dataset(dataset_id)
+    dataset.location = REGION
+
+    dataset = client.create_dataset(dataset, exists_ok=True)
+    print(f"Dataset {dataset_id} created in {dataset.location}")
+
+    # Create table
+    with open("sql/vitals_schema.sql") as f:
+        sql = f.read()
+
+    sql = sql.replace("{{PROJECT_ID}}", PROJECT_ID).replace(
+        "{{DATASET}}", DATASET)
+    client.query(sql).result()
+
+    print("Successfully created BigQuery table.")
 
 
-# Create table
-with open("sql/vitals_schema.sql") as f:
-    sql = f.read()
-
-sql = sql.replace("{{PROJECT_ID}}", PROJECT_ID).replace(
-    "{{DATASET}}", DATASET)
-client.query(sql).result()
-
-print("Successfully created BigQuery table.")
+if __name__ == "__main__":
+    main()
