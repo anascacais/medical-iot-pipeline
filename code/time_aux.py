9866496bc2695f84d3b28f753eb03772e97578e3
anascacais <anascacais@gmail.com>
2026-01-29 18:18:20 +0100

    handle ts and dt

---
+++ code/time_aux.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timezone
+
+
+def dt2ts(dt):
+    """
+    Convert a timezone-aware UTC datetime to a Unix timestamp in milliseconds. Sub-milisecond precision is truncated.
+
+    Parameters
+    ----------
+    dt : datetime.datetime
+        Timezone-aware datetime object (UTC) representing the event time.
+
+    Returns
+    -------
+    int
+        Unix timestamp in milliseconds since the epoch (UTC).
+    """
+    return int(dt.timestamp() * 1000)
+
+
+def ts2dt(ts):
+    """
+    Convert a Unix timestamp in milliseconds to a timezone-aware UTC datetime.
+
+    Parameters
+    ----------
+    ts : int
+        Unix timestamp in milliseconds since the epoch (UTC).
+
+    Returns
+    -------
+    datetime.datetime
+        Timezone-aware datetime object in UTC corresponding to the input
+        timestamp.
+    """
+    return datetime.fromtimestamp(ts / 1000, tz=timezone.utc)
